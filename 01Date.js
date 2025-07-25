f07d4cd7c25e1cec5443e0d82dcd4f0e7a7f9823
SiddhantShukla02 <77145400+SiddhantShukla02@users.noreply.github.com>
2025-07-26 01:38:33 +0530

    working with Date object

---
+++ 01Date.js
@@ -0,0 +1,13 @@
+let myDate = new Date();
+
+// console.log(myDate);
+
+// console.log(myDate.getTime());
+
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(myDate.toLocaleString())
+
+let date2 = Date.now();
+
+console.log(date2);
